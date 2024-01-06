84953ea2f45fb8a37f258de66899f292f0389faf
MarceloGustavoCordoba <marcelo.cordoba@live.com.ar>
2024-01-06 12:44:28 -0300

    asd

--- main.py
+++ main.py
@@ -17,3 +17,10 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put('/item/{item_id}')
 def update_item(item_id: int, item: Item):
     return {'items_name': item.name, 'item_id': item_id}
+
+@app.post("/webhook")
+def webhook():
+    # Puedes realizar cualquier lógica necesaria aquí
+    
+    # Respuesta con código de estado 200
+    return {"message": "OK"}
