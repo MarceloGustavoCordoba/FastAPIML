b64fd30e6286237fadf16e61ba2c43a84ba97b20
MarceloGustavoCordoba <marcelo.cordoba@live.com.ar>
2024-01-06 12:18:38 -0300

    creado modelo item

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from typing import Union
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 @app.get('/')
 def read_root():
